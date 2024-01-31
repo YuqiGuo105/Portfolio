8cc482464317524a884d97700a853ae953e14e47
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2024-01-30 22:00:07 -0500

    Update supabaseClient.js

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Ensure these variable names match with those in your .env file
 // If these are used client-side, make sure they are prefixed with NEXT_PUBLIC_
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://iyvhmpdfrnznxgyvvkvx.supabase.co';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5dmhtcGRmcm56bnhneXZ2a3Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYwNjcyMjMsImV4cCI6MjAyMTY0MzIyM30.zwq9WBVBLmFaUnA2PBU9hanYfmJYMxfg4l37wXEf1NI';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
