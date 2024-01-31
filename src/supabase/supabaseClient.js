54808c2b7025e30d83d2cb5b6bc5f67cf86c331e
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2024-01-30 21:35:41 -0500

    Update supabaseClient.js

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+// Ensure these variable names match with those in your .env file
+// If these are used client-side, make sure they are prefixed with NEXT_PUBLIC_
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
