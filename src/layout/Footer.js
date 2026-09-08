d71d487254f329be80d6540031da4f2189dfa755
YuqiGuo105 <131561736+YuqiGuo105@users.noreply.github.com>
2026-09-07 23:02:53 -0600

    revert(ui): restore footer author credit

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,14 +1,6 @@
-import Link from 'next/link';
-
 import SocialLinks from '../components/SocialLinks';
-import styles from './Footer.module.css';
-
-const LICENSE_URL = 'https://github.com/YuqiGuo105/Portfolio/blob/main/LICENSE';
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
-  const years = currentYear > 2023 ? `2023–${currentYear}` : '2023';
-
   return (
     <div className="footer">
       <div className="footer__builder">
@@ -18,17 +10,14 @@ const Footer = () => {
               <SocialLinks className="social-links" />
             </div>
             <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 align-center">
-              <div className={`copyright-text ${styles.legalCopy}`}>
-                <span>© {years} <strong>Yuqi Guo</strong>. All rights reserved.</span>
-                <span className={styles.licenseNote}>Content and source code are licensed separately.</span>
+              <div className="copyright-text">
+                © 2023 <strong>Yuqi Guo&apos;s Blog</strong> All Rights Reserved
               </div>
             </div>
             <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 align-right">
-              <nav className={`copyright-text ${styles.legalLinks}`} aria-label="Legal information">
-                <Link href="/content-usage">Content Usage</Link>
-                <a href={LICENSE_URL} target="_blank" rel="noreferrer">Open Source Licenses</a>
-                <Link href="/privacy">Privacy</Link>
-              </nav>
+              <div className="copyright-text">
+                Developed by <strong>Yuqi Guo</strong>
+              </div>
             </div>
           </div>
         </div>
